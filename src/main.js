bbd221c2938f9b35773897c1400b19f7b9aa170a
Pipe-Runner <piperunner.in@gmail.com>
2022-09-30 18:40:02 +0200

    Completed task 3

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 // firstImage();
 // blueWhiteGradient();
-redSphere();
-//normalsSphere();
+// redSphere();
+normalsSphere();
 //sphereAndGround();
 //antialiasing();
 //diffuseSphere();
@@ -104,7 +104,41 @@ function redSphere() {
 }
 
 function normalsSphere() {
-  //TODO
+  // Screen space
+  const aspectRatio = 16.0 / 9.0;
+  const imageWidth = 400;
+  const imageHeight = Number.parseInt(imageWidth / aspectRatio, 10);
+
+  // View port
+  const origin = new Vec3(0, 0, 0);
+  const viewportHeight = 2;
+  const focalLength = 1.0;
+  const viewportWidth = aspectRatio * viewportHeight;
+  const horizontal = new Vec3(viewportWidth, 0, 0);
+  const vertical = new Vec3(0, viewportHeight, 0);
+  const lowerLeftCorner = origin
+    .subtract(horizontal.multiply(0.5))
+    .subtract(vertical.multiply(0.5))
+    .subtract(new Vec3(0, 0, focalLength));
+
+  const image = [];
+
+  for (let j = imageHeight - 1; j >= 0; --j) {
+    for (let i = 0; i < imageWidth; ++i) {
+      const u = i / (imageWidth - 1);
+      const v = j / (imageHeight - 1);
+      const r = new Ray(
+        origin,
+        lowerLeftCorner
+          .add(horizontal.multiply(u))
+          .add(vertical.multiply(v))
+          .subtract(origin)
+      );
+      image.push(rayToColorV3(r).toList());
+    }
+  }
+
+  displayImage(imageWidth, imageHeight, image);
 }
 
 function sphereAndGround() {
@@ -139,7 +173,7 @@ function rayToColorV1(ray) {
 }
 
 function rayToColorV2(ray) {
-  if (hitSphere(new Vec3(0, 0, -1), 0.5, ray)) {
+  if (hitSphereV1(new Vec3(0, 0, -1), 0.5, ray)) {
     return new Vec3(1, 0, 0);
   }
 
@@ -154,7 +188,29 @@ function rayToColorV2(ray) {
     .add(new Vec3(0.5, 0.7, 1.0).multiply(t));
 }
 
-function hitSphere(center, radius, ray) {
+function rayToColorV3(ray) {
+  const sphereCenter = new Vec3(0, 0, -1);
+  const tComputed = hitSphereV2(sphereCenter, 0.5, ray)
+  if (tComputed !== -1) {
+    const N = ray.at(tComputed).subtract(sphereCenter).unitVector();
+    const r = (N.x + 1) * 0.5;
+    const g = (N.y + 1) * 0.5;
+    const b = (N.z + 1) * 0.5;
+    return new Vec3(r, g, b);
+  }
+
+  const unitDirection = ray.direction.unitVector();
+
+  // Scaling t between 0 and 1
+  const t = 0.5 * (unitDirection.y + 1.0);
+
+  // Linear interpolation (lerp) between white and blue
+  return new Vec3(1.0, 1.0, 1.0)
+    .multiply(1.0 - t)
+    .add(new Vec3(0.5, 0.7, 1.0).multiply(t));
+}
+
+function hitSphereV1(center, radius, ray) {
   const oc = ray.origin.subtract(center);
 
   const a = ray.direction.dot(ray.direction);
@@ -164,4 +220,20 @@ function hitSphere(center, radius, ray) {
   const D = b * b - 4 * a * c;
 
   return D >= 0;
+}
+
+function hitSphereV2(center, radius, ray) {
+  const oc = ray.origin.subtract(center);
+
+  const a = ray.direction.dot(ray.direction);
+  const b = 2 * oc.dot(ray.direction);
+  const c = oc.dot(oc) - radius * radius;
+
+  const D = b * b - 4 * a * c;
+
+  if(D >= 0){
+    return (-b - Math.sqrt(D)) / (2 * a);
+  }
+
+  return -1;
 }
